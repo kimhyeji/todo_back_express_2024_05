4197e06676b87ec023f894021e172a7551028b17
kimhyeji <gpwl5589@gmail.com>
2024-05-31 19:25:11 +0900

    리스트 조회

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 import express from "express";
 import cors from "cors";
+import mysql from "mysql2/promise";
+
 const app = express();
 const port = 3000;
+
+const pool = mysql.createPool({
+    host: "localhost",
+    user: "korad",
+    password: "kor123414",
+    database: "todo_2024_05",
+    waitForConnections: true,
+    queueLimit: 0,
+    dateStrings: true,
+});
 
 var corsOptions = {
     origin: "https://cdpn.io",
@@ -10,8 +22,24 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.get("/", (req, res) => {
-    res.send("Hello world");
+app.get("/:user_code/todos", async (req, res) => {
+    const { user_code } = req.params;
+
+    const [rows] = await pool.query(
+        `
+        SELECT *
+        FROM todo
+        WHERE user_code = ?
+        ORDER BY id DESC
+        `,
+        [user_code]
+    );
+
+    res.json({
+        resultCode: "S-1",
+        msg: "성공",
+        data: rows
+    });
 });
 
 app.get("/todos", (req, res) => {
